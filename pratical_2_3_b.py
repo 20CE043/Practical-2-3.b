6c097c0faa2da67303b1d820f947e1496932a76a
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 11:17:33 +0530

    Add files via upload

---
+++ pratical_2_3_b.py
@@ -0,0 +1,9 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Github link: https://github.com/20CE043/Practical-2-3.b.git
+#Aim: Write a Python program to remove an item from a set if it is present in the set.
+t={1,2,3,4,5,6}
+#removing element from the set
+t.remove(2)
+t.remove(4)
+print(t)
